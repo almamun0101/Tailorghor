d13fa880e6e3c682fdf14d4f8d27bc1ddecf5e85
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-04-06 01:12:36 +0600

    ain

--- src/School/SchoolDress.jsx
+++ src/School/SchoolDress.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-const genders = ["Male ", "Female "];
+
+
 const sizes = ["ES","S","M","L","X","XL"]
-const images = [
-  {}
-]
+
 export const SchoolDress = ({schoolName }) => {
+  const [gender,setGender]=useState("girl");
+  
+  const handleChange = (e)=>{
+    setGender(e.target.value);
+  }
+
+  const imgUrl = 
+  gender==="boy"
+  ?"./boy.png"
+  :"./girl.png"
 
   return (
     <div className="pt-20">
@@ -14,14 +23,15 @@ export const SchoolDress = ({schoolName }) => {
           <h2 className="text-lg p-1">Uniform For {schoolName}</h2>
         </div>
         <div className="flex gap-2 w-full text-black text-center justify-around">
-         <select name="" id="gender" className="border w-1/2 rounded-2xl p-2">
-            <option value="">Select Gender</option>
-            {
-              genders.map((gender)=>(
-              <option key={gender} value={gender}>{gender}</option>
-            ))
-            }
-         </select>
+        <select
+        value={gender}
+        onChange={handleChange}
+        className="border w-1/2 rounded-2xl p-2"
+      >
+        {/* <option value="boy">Select Gender</option> */}
+        <option value="girl">Girl</option>
+        <option value="boy">Boy</option>
+      </select>
           <select name="" id="size" className="border w-1/2 rounded-2xl p-2">
             <option value="Select Size">Select Size</option>
             {
@@ -34,7 +44,7 @@ export const SchoolDress = ({schoolName }) => {
         </div>
         <div className="w-full py-4">
 
-          <img src="./boy.png" alt="" />
+          <img src={imgUrl} alt="" />
         </div>
         <div className="flex gap-2">
           <button className="bg-pri w-1/2 rounded-2xl p-2  text-sec text-lg font-bold">Order</button>
